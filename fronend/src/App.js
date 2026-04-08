5703ee2bdca553915df5ac17d14225870c3a8deb
Matic Gradišar <mato.gradisar@gmail.com>
2026-04-08 12:21:51 +0200

    first commit

---
+++ fronend/src/App.js
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+
+function App() {
+
+  //STATES
+
+ const[client, setClient] = useState("");
+  const[amount, setAmount] = useState("");
+  const [description, setDescription] = useState("");
+  const [invoices, setInvoices] = useState([]);
+
+  //CREATE
+ 
+
+  const getInvoices = async () => {
+    const res = await
+     fetch("http://localhost:3000/invoices"
+      , { 
+    
+      method: "POST",
+      headers:{
+         "Content-Type":  "application/json",
+      },
+      body: JSON.stringify({
+        client,
+        amount: Number(amount),
+        description
+      }),
+    });
+    
+    const data =await res.json();
+    console.log(data);
+  
+    getInvoices();
+  };
+
+  //GET
+   
+    setInvoices(data);
+  return (
+    <div style={{padding: 20}}>
+      <h1>AI računovdstvo </h1>
+  
+      <input
+      placeholder="client"
+      value={client}
+      onChange={(e) => setClient(e.target.value)}
+      />
+      <br /> <br />
+
+      <input
+      placeholder="amount"
+      value={amount}
+      onChange={(e) => setAmount(e.target.value)} 
+      />
+      <br /><br />
+
+      <input
+      placeholder="Desription"
+      value={description}
+      onChange={(e) => setDescription(e.target.value)}
+      />
+
+      <button onClick={createInvoice}>
+         createInvoice
+      </button>
+      
+
+      <h2>invoices:</h2>
+      {invoices.map((inv, index) => (
+        <div key={index}>
+         <p><b>{inv.client}</b></p>
+         <p>{inv.amount}</p>
+         <p>{inv.description}</p>
+         <hr />
+    </div>
+      ))}
+   </div>
+  );
+};
+export default App; 
---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// "baza"
+let invoices = [];
+
+
+// CREATE invoice
+app.post("/invoices", (req, res) => {
+    const { client, amount, 
+        description } = req.body;
+
+        if (!client || !amount) {
+            return res.status(400).json({
+                massage: "Client and amount are required"
+            });
+            getInvoices();
+        }
+
+const newInvoice = {
+    id: Date.now(),
+    client, 
+    amount, 
+    description, 
+    date: new Date(), 
+    invoiceNumber:"2026-" + 
+    (invoices.length + 1)
+};
+
+    invoices.push(newInvoice);
+
+res.json(newInvoice);
+});
+
+// READ 
+app.get("/invoices", (req, res) => {res.json(invoices);});
+
+app.listen(3000, () => {console.log("Server running on http://localhost:3000");});
+
+// DELETE 
+app.delete("/invoices/:id", (req, res) => {
+    const id = parseInt(req,URLSearchParams.id);
+
+    invoices = invoices.filter(invoice => invoice.id !== id);
+
+    res.json({
+        message: "Invoice deleted",
+        remaining: invoices.length
+    });
+});
+
+//START SERVER 
+app.listen(3000, () => {console.log("Server running on http://localhost:3000");});
